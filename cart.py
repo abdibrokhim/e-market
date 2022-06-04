00b453423e31f67f0171be861e38abe3c234e911
abdibrokhim <abdibrokhim@gmail.com>
2022-06-05 00:11:36 +0500

    updated, improved, in development ...

--- cart.py
+++ cart.py
@@ -1,25 +1,33 @@
 import sqlite3
+
 connection = sqlite3.connect('market.db')
 cursor = connection.cursor()
 
 
 class Cart:
     cart = []
-    total = 0
+    total = {}
     cost = 0
 
-    def __init__(self, table_name, item_name):
+    def __init__(self, table_name):
         self.table_name = table_name
-        self.item_name = item_name
 
-    def add_to_cart(self, cart, total, cost):
-        item = cursor.execute("SELECT NAME, SUPPLY, PRICE FROM {}".format(self.table_name)).fetchall()
+    def add_to_cart(self, item_name, item_total, price=0):
+        # print("\n:: IN STOCK ::")
+        # item_name = str(input("Input: "))
+        # item_total = int(input("Input: "))
+
+        item = cursor.execute("SELECT NAME, SUPPLY, PRICE, CATEGORY FROM {}".format(self.table_name)).fetchall()
         for i in range(0, len(item)):
-            if self.item_name == item[i][0]:
-                cart.append(self.item_name)
-                total += 1
-                cost += total * item[i][3]
-        print(cart, total, cost)
+            if item_name == item[i][0]:
+                self.cart.append(item_name)
+                self.total[item_name] = item_total
+                price += item_total * item[i][2]
+                self.cost += price
+        print("ALL INFO\n", self.cart, self.total, self.cost)
+
+    def get_cart(self,):
+        print("CART\n", self.cart, self.cost)
 
     def delete_from_cart(self):
         pass
--- main.py
+++ main.py
@@ -3,14 +3,23 @@ from shop import Shop
 
 
 def main():
-
     item_1 = Shop('FOOD', 'RED BULL', 10, 1.99, 'ENERGY DRINKS')
     item_2 = Shop('FOOD', 'MONSTER', 10, 1.49, 'ENERGY DRINKS')
     item_3 = Shop('FOOD', 'BANG', 10, 2.29, 'ENERGY DRINKS')
     item_4 = Shop('FOOD', 'ROCKSTAR', 10, 1.79, 'ENERGY DRINKS')
 
-    shopper = Cart('FOOD', 'RED BULL')
-    
+    shop1 = Cart('FOOD')
+    shop2 = Cart('FOOD')
+    shop3 = Cart('FOOD')
+
+    shop1.add_to_cart('MONSTER', 2)
+    shop2.add_to_cart('RED BULL', 2)
+    shop3.add_to_cart('RED BULL', 2)
+
+    shop1.get_cart()
+    shop2.get_cart()
+    shop3.get_cart()
+
     # item_5.new_table('DRINK')
 
     # item_1.add_item()
@@ -18,10 +27,8 @@ def main():
     # item_3.add_item()
     # item_4.add_item()
 
-    item_1.get_item()
-    print("\n")
-    item_1.get_table()
-
+    # item_1.get_item()
+    # item_1.get_table()
 
     # c = 0
     # while True:
