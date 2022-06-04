e2de3fba321ae28495a1d916875f88443b987265
abdibrokhim <abdibrokhim@gmail.com>
2022-06-05 02:09:13 +0500

    updated, improved, in development ...

--- cart.py
+++ cart.py
@@ -6,13 +6,13 @@ cursor = connection.cursor()
 
 class Cart:
     cart = []
-    total = {}
-    cost = 0
+    total = []
+    cost = []
 
     def __init__(self, table_name):
         self.table_name = table_name
 
-    def add_to_cart(self, item_name, item_total, price=0):
+    def add_to_cart(self, item_name, item_total):
         # print("\n:: IN STOCK ::")
         # item_name = str(input("Input: "))
         # item_total = int(input("Input: "))
@@ -21,13 +21,20 @@ class Cart:
         for i in range(0, len(item)):
             if item_name == item[i][0]:
                 self.cart.append(item_name)
-                self.total[item_name] = item_total
-                price += item_total * item[i][2]
-                self.cost += price
+                self.total.append(item_total)
+                self.cost.append(item_total * item[i][2])
         print("ALL INFO\n", self.cart, self.total, self.cost)
 
     def get_cart(self,):
-        print("CART\n", self.cart, self.cost)
+        print("\nCART\n", self.cart, self.total, self.cost)
 
-    def delete_from_cart(self):
-        pass
+    def delete_from_cart(self, item_name, item_total):
+        item = cursor.execute("SELECT NAME, SUPPLY, PRICE, CATEGORY FROM {}".format(self.table_name)).fetchall()
+        for i in self.cart:
+            if item_name == i:
+                # self.cart.remove(item_name)
+                self.total.insert(i, self.total[i] - item_total)
+                if self.total.insert(i, self.total[i] - item_total) == 0:
+                    self.cart.remove(item_name)
+                self.cost.insert(i, self.cost[i] - item_total * item[2])
+        print("ALL INFO\n", self.cart, self.total, self.cost)
--- main.py
+++ main.py
@@ -8,24 +8,29 @@ def main():
     item_3 = Shop('FOOD', 'BANG', 10, 2.29, 'ENERGY DRINKS')
     item_4 = Shop('FOOD', 'ROCKSTAR', 10, 1.79, 'ENERGY DRINKS')
 
-    shop1 = Cart('FOOD')
-    shop2 = Cart('FOOD')
-    shop3 = Cart('FOOD')
-
-    shop1.add_to_cart('MONSTER', 2)
-    shop2.add_to_cart('RED BULL', 2)
-    shop3.add_to_cart('RED BULL', 2)
-
-    shop1.get_cart()
-    shop2.get_cart()
-    shop3.get_cart()
-
     # item_5.new_table('DRINK')
 
     # item_1.add_item()
     # item_2.add_item()
     # item_3.add_item()
     # item_4.add_item()
+
+    shop1 = Cart('FOOD')
+    shop2 = Cart('FOOD')
+    shop3 = Cart('FOOD')
+
+    shop1.add_to_cart('MONSTER', 2)
+    shop1.add_to_cart('RED BULL', 2)
+    shop1.add_to_cart('RED BULL', 2)
+
+    shop1.delete_from_cart('RED BULL', 1)
+
+    # shop2.add_to_cart('BANG', 3)
+
+    shop1.get_cart()
+    # shop2.get_cart()
+    # shop3.get_cart()
+
 
     # item_1.get_item()
     # item_1.get_table()
